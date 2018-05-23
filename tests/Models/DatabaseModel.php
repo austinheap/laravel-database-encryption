883af22b57aab914851d69fa8b18c77ccf30a289
Austin Heap <me@austinheap.com>
2018-05-23 11:16:43 -0700

    draft support for float(double)/int re: issue #18

--- tests/Models/DatabaseModel.php
+++ tests/Models/DatabaseModel.php
@@ -5,7 +5,7 @@
  * @package     laravel-database-encryption
  * @link        https://github.com/austinheap/laravel-database-encryption
  * @author      Austin Heap <me@austinheap.com>
- * @version     v0.1.0
+ * @version     v0.1.2
  */
 
 namespace AustinHeap\Database\Encryption\Tests\Models;
@@ -19,7 +19,20 @@ class DatabaseModel extends RealModel
 {
     use HasEncryptedAttributes;
 
-    public    $table     = 'test_models';
-    protected $fillable  = ['shouldnt_be_encrypted', 'should_be_encrypted'];
-    protected $encrypted = ['should_be_encrypted'];
+    public $table = 'test_models';
+
+    protected $fillable = [
+        'should_be_encrypted',
+        'shouldnt_be_encrypted',
+        'should_be_encrypted_float',
+        'shouldnt_be_encrypted_float',
+        'should_be_encrypted_int',
+        'shouldnt_be_encrypted_int',
+    ];
+
+    protected $encrypted = [
+        'should_be_encrypted',
+        'should_be_encrypted_float',
+        'should_be_encrypted_int',
+    ];
 }
---
+++ tests/Traits/FloatTest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * tests/Traits/FloatTest.php
+ *
+ * @package     laravel-database-encryption
+ * @link        https://github.com/austinheap/laravel-database-encryption
+ * @author      Austin Heap <me@austinheap.com>
+ * @version     v0.1.2
+ */
+
+namespace AustinHeap\Database\Encryption\Tests\Traits;
+
+use AustinHeap\Database\Encryption\Tests\DatabaseTestCase;
+use AustinHeap\Database\Encryption\Tests\Models\DatabaseModel;
+
+/**
+ * FloatTest
+ */
+class FloatTest extends DatabaseTestCase
+{
+    public function testCreate()
+    {
+        $model = DatabaseModel::create($this->randomValues());
+
+        $this->assertFalse(empty($model->should_be_encrypted_float));
+    }
+
+    public function testShouldBeEncryptedFloat()
+    {
+        $model = DatabaseModel::create($this->randomValues());
+
+        $this->assertTrue($model->exists);
+        $this->assertNotFalse(strpos($model->getOriginal('should_be_encrypted_float'), '__LARAVEL-DATABASE-ENCRYPTED-'));
+        $this->assertFalse(is_int($model->should_be_encrypted_float));
+        $this->assertTrue(is_float($model->should_be_encrypted_float));
+        $this->assertTrue(is_double($model->should_be_encrypted_float));
+        $this->assertFalse(is_string($model->should_be_encrypted_float));
+    }
+
+    public function testShouldntBeEncryptedFloat()
+    {
+        $model = DatabaseModel::create($this->randomValues());
+
+        $this->assertTrue($model->exists);
+        $this->assertFalse(is_int($model->shouldnt_be_encrypted_float));
+        $this->assertTrue(is_float($model->shouldnt_be_encrypted_float));
+        $this->assertTrue(is_double($model->shouldnt_be_encrypted_float));
+        $this->assertFalse(is_string($model->should_be_encrypted_float));
+    }
+}
